f305dc098201e4a417dfb4f2e0ec6859fe3a8fd0
ALEX_ASKREN <askrap01@pfw.edu>
2025-04-01 11:17:08 -0400

    mem update

--- 06_data_mem.v
+++ 06_data_mem.v
@@ -1,7 +1,8 @@
 // DATA MEMORY MODULE
 // This module implements data memory with read and write capabilities.
 module mem_data #(
-    parameter MEM_SIZE = 1024,       // Size of the memory in words (default: 1024)
+    parameter MEM_SIZE_BOTTOM = 1024,       // Size of the memory in words (default: 1024)
+    parameter MEM_SIZE_TOP = 1050,       // Size of the memory in words (default: 1024)   
     parameter DATA_WIDTH = 32,       // Width of the data in bits (default: 32 bits)
     parameter ADDR_WIDTH = 32        // Address width (default: 32 bits)
 )(
@@ -14,14 +15,14 @@ module mem_data #(
 );
 
     // Define a memory array of size MEM_SIZE, where each word is of DATA_WIDTH
-    reg [DATA_WIDTH-1:0] memory [0:MEM_SIZE-1];  // Memory size based on MEM_SIZE parameter
+    reg [DATA_WIDTH-1:0] memory [MEM_SIZE_BOTTOM:MEM_SIZE_TOP-1];  // Memory size based on MEM_SIZE parameter
     
     // Initialize the memory to zero during reset
     integer i;
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             // Reset memory contents to 0
-            for (i = 0; i < MEM_SIZE; i = i + 1) begin
+            for (i = 0; i < MEM_SIZE_BOTTOM - MEM_SIZE_TOP; i = i + 1) begin
                 memory[i] <= {DATA_WIDTH{1'b0}};  // Reset to 0 for the given data width
             end
         end else if (wr_en) begin
@@ -33,7 +34,7 @@ module mem_data #(
     // Read operation (load data) - this occurs when wr_en is NOT asserted
     always @(posedge clk) begin
         if (~wr_en) begin
-            if (addr < MEM_SIZE) begin
+            if ((addr >= MEM_SIZE_BOTTOM) && (addr < MEM_SIZE_TOP-1) ) begin
                 dout <= memory[addr[ADDR_WIDTH-1:2]];  // Read data from memory
             end else begin
                 dout <= {DATA_WIDTH{1'b0}};  // Handle invalid address by returning 0
